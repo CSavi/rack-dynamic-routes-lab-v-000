861a44eb726699acdc6d86b2d4a0c8ca2ad8705a
Carrie Savinell <carrielane89@gmail.com>
2018-07-05 16:58:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,9 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
 
-      resp write item.price
+      if item.include?(item)
+        resp write item.price
+      else   
 
       resp.write "Route not found"
       resp.status = 404
