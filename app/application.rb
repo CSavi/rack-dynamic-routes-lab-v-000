244b899e4d50ad4827445eb903e69fe17e089efc
Carrie Savinell <carrielane89@gmail.com>
2018-07-05 16:48:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack:Request.new(env)
 
-    unless req.path == "/items"
+    if req.path == "/items"
       resp.write "Route not found"
       resp.status = 404
     else
